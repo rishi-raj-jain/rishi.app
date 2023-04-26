861bac93b61ba99267a38d9fd8aa350b8e584809
Rishi Raj Jain <rjain@llnw.com>
2023-04-26 12:49:46 +0530

    cache pages

--- routes.cjs
+++ routes.cjs
@@ -4,9 +4,25 @@ const router = new Router()
 
 router.static('.vercel/output/static')
 
+const paths = ['/', '/about', '/cv', '/storyblok', '/blogs']
+
+paths.forEach((i) => {
+	router.match(i, ({ cache, renderWithApp }) => {
+		cache({
+			browser: false,
+			edge: {
+				maxAgeSeconds: 60,
+				staleWhileRevalidateSeconds: 60 * 60 * 24 * 365
+			}
+		})
+		renderWithApp()
+	})
+})
+
 router.fallback(({ renderWithApp, removeUpstreamResponseHeader, cache }) => {
 	removeUpstreamResponseHeader('cache-control')
 	cache({
+		edge: false,
 		browser: false
 	})
 	renderWithApp()
