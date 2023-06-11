3b80aba20f88c29d281240ea7be1927205e70c9e
Rishi Raj Jain <rjain@llnw.com>
2023-06-11 22:24:55 +0530

    Rename title string

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,7 @@
 	import Seo from '@/src/components/Seo.svelte'
 </script>
 
-<Seo title={`About - Rishi Raj Jain`} />
+<Seo title="About - Rishi Raj Jain" />
 
 <div class="flex w-full flex-col items-center text-[14px]">
 	<div class="mt-10 flex w-[90vw] max-w-[540px] flex-col">
