ed2c6c970d66c23f17b825e102687da380fc06ab
Rishi Raj Jain <rjain@llnw.com>
2023-06-11 16:17:34 +0530

    update SEO

--- src/components/Seo.svelte
+++ src/components/Seo.svelte
@@ -6,6 +6,19 @@
 		domain: string = 'https://sveltekit.rishi.app',
 		image: string = '/static/social-media-card.jpg',
 		pathname: string = $page.url.pathname
+
+	let seoImage = ''
+
+	function updateSEOImage(src: string) {
+		const temp = src.replace('https://', '//').replace('//', 'https://')
+		if (temp.startsWith('https')) {
+			seoImage = temp
+		} else {
+			seoImage = domain + temp
+		}
+	}
+
+	$: updateSEOImage(image)
 </script>
 
 <svelte:head>
@@ -21,13 +34,13 @@
 	<!-- Open Graph / Facebook -->
 	<meta property="og:title" content={title} />
 	<meta property="og:type" content="website" />
-	<meta property="og:description" content={description} />
-	<meta property="og:image" content={domain + image} />
+	<meta property="og:image" content={seoImage} />
 	<meta property="og:url" content={domain + pathname} />
+	<meta property="og:description" content={description} />
 	<!-- Twitter -->
 	<meta property="twitter:title" content={title} />
+	<meta property="twitter:image" content={seoImage} />
+	<meta property="twitter:url" content={domain + pathname} />
 	<meta property="twitter:description" content={description} />
 	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:image" content={domain + image} />
-	<meta property="twitter:url" content={domain + pathname} />
 </svelte:head>
