9e1443e345aa43b34869168ab4baed4751d4ece7
Rishi Raj Jain <rjain@llnw.com>
2023-06-17 22:17:04 +0530

    homepage design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,40 +17,39 @@
 			class="aspect-square h-[300px] w-[300px] transform-gpu rounded"
 			style={`background-image: url(${data.blurImage}); background-repeat: no-repeat; background-position: 50% 50%; background-size: cover;`}
 		/>
-		<div class="flex flex-col items-start">
+		<div class="flex flex-col">
 			<h1 class="mt-8 text-2xl font-bold sm:text-5xl lg:mt-0">Rishi Raj Jain</h1>
+			{@html data.tagline}
 			<div class="mt-6 h-[1px] w-full" />
 			<Socials />
-			<div class="mt-6 h-[1px] w-full bg-gray-200 dark:bg-gray-700" />
-			{@html data.tagline}
+			<ul class="font-sm mt-4 flex flex-col space-x-0 space-y-2 text-neutral-500 dark:text-neutral-400 md:flex-row md:space-x-4 md:space-y-0 lg:mt-auto">
+				<li>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://twitter.com/intent/follow?screen_name=rishi_raj_jain_"
+						class="flex items-center transition-all hover:text-neutral-700 dark:hover:text-neutral-200"
+					>
+						<svg class="mr-2" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor" />
+						</svg>
+						<p class="h-7">follow me on twitter</p>
+					</a>
+				</li>
+				<li>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://substack.com/@rishirajjain"
+						class="flex items-center transition-all hover:text-neutral-700 dark:hover:text-neutral-200"
+					>
+						<svg class="mr-2" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor" />
+						</svg>
+						<p class="h-7">get email updates</p>
+					</a>
+				</li>
+			</ul>
 		</div>
 	</div>
-	<ul class="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-500 dark:text-neutral-400 md:flex-row md:space-x-4 md:space-y-0">
-		<li>
-			<a
-				target="_blank"
-				rel="noopener noreferrer"
-				href="https://twitter.com/intent/follow?screen_name=rishi_raj_jain_"
-				class="flex items-center transition-all hover:text-neutral-700 dark:hover:text-neutral-200"
-			>
-				<svg class="mr-2" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor" />
-				</svg>
-				<p class="h-7">follow me on twitter</p>
-			</a>
-		</li>
-		<li>
-			<a
-				target="_blank"
-				rel="noopener noreferrer"
-				href="https://substack.com/@rishirajjain"
-				class="flex items-center transition-all hover:text-neutral-700 dark:hover:text-neutral-200"
-			>
-				<svg class="mr-2" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor" />
-				</svg>
-				<p class="h-7">get email updates</p>
-			</a>
-		</li>
-	</ul>
 </div>
