ecef8c7c0f91c993fc6742dd091c29dca470e889
Rishi Raj Jain <rjain@llnw.com>
2023-06-19 17:34:45 +0530

    improve logic

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -14,19 +14,19 @@
 <h1 class="text-2xl font-bold sm:text-5xl">Showcase</h1>
 <div class="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-2">
 	{#each groups as group}
-		{#if group % 2 === 0}
-			<Preview gap={false} {...data.items[group]} />
+		{#if (group * 3) % 2 === 0}
+			<Preview gap={false} {...data.items[group * 3]} />
 		{/if}
-		{#if data.items.length > group + 1}
+		{#if data.items.length > group * 3 + 1}
 			<div class="flex w-full flex-col gap-4">
-				<Preview {...data.items[group + 1]} />
-				{#if data.items.length > group + 2}
-					<Preview {...data.items[group + 2]} />
+				<Preview {...data.items[group * 3 + 1]} />
+				{#if data.items.length > group * 3 + 2}
+					<Preview {...data.items[group * 3 + 2]} />
 				{/if}
 			</div>
 		{/if}
-		{#if group % 2 !== 0}
-			<Preview gap={false} {...data.items[group]} />
+		{#if (group * 3) % 2 !== 0}
+			<Preview gap={false} {...data.items[group * 3]} />
 		{/if}
 	{/each}
 	{#if data.items.length > countGroups * 3}
