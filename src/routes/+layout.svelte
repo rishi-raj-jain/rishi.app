0f50f046152185a02a3ccba2cb2fefd7d8b3f828
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-06-09 20:12:41 +0530

    remove footer impot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '@/src/app.css'
 	import { navigating } from '$app/stores'
-	import Footer from '@/src/components/Footer.svelte'
 	import Header from '@/src/components/Header.svelte'
 </script>
 
