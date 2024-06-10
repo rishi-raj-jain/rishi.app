bc9e61cc8fdc85103181c053df8b472546493304
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-06-10 06:59:03 +0530

    remove data fetching

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,6 @@
 import { env } from '$env/dynamic/private'
-import { load as loadCheerio } from 'cheerio'
-import renderRichText from '@/src/lib/render'
 import type { PageServerLoad } from './$types'
-import { getTagline } from '@/src/lib/storyblok'
 
-export const load: PageServerLoad = async ({ url, fetch }) => {
-	let tagline = await getTagline('home')
-	tagline = renderRichText.render(tagline)
-	const $ = loadCheerio(tagline)
-	$('p').addClass('mt-6 text-lg font-light text-slate-600')
-	// @ts-ignore
-	tagline = $('p').parent().html()
-	// Use ImageKit transformation to blur the URL and convert the blurred image to a string
-	const call = await fetch('https://ik.imagekit.io/vjeqenuhn/static/favicon-image.jpg?tr=bl-50')
-	const resp = await call.arrayBuffer()
-	let blurImage = Buffer.from(resp).toString('base64')
-	blurImage = 'data:image/jpeg;base64,' + blurImage
-	return { tagline, domain: env.DOMAIN ?? url.origin, blurImage }
+export const load: PageServerLoad = async ({ url }) => {
+	return { domain: env.DOMAIN ?? url.origin }
 }
