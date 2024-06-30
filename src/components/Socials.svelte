085c93531ec65a7435bfe6b0873a2e1229377e79
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-07-01 00:05:39 +0530

    icons

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -10,24 +10,24 @@
 
 <div class="flex flex-row items-center space-x-5 text-gray-400">
 	<a href="https://github.com/rishi-raj-jain" target="_blank">
-		<GitHubIcon height="25px" width="25px" />
+		<GitHubIcon height="20px" width="20px" />
 	</a>
 	<a href="https://twitter.com/rishi_raj_jain_" target="_blank">
-		<TwitterIcon />
+		<TwitterIcon font-size="13px" />
 	</a>
 	<a href="https://medium.com/@rishi-raj-jain" target="_blank">
-		<MediumIcon height="30px" width="30px" color="black" />
+		<MediumIcon height="20px" width="20px" color="black" />
 	</a>
 	<a href="https://behance.net/rishi-raj-jain" target="_blank">
-		<BehanceIcon height="30px" width="30px" color="#0057ff" />
+		<BehanceIcon height="20px" width="20px" color="#0057ff" />
 	</a>
 	<a href="https://linkedin.com/in/rishi-raj-jain" target="_blank">
-		<LinkedInIcon height="26px" width="26px" />
+		<LinkedInIcon height="20px" width="20px" />
 	</a>
 	<a href="https://polywork.rishi.app" target="_blank">
-		<PolyworkIcon height="26px" width="26px" />
+		<PolyworkIcon height="20px" width="20px" />
 	</a>
 	<a href="https://peerlist.io/rishi_raj_jain_" target="_blank">
-		<PeerlistIcon height="29px" width="29px" color="#219653" />
+		<PeerlistIcon height="20px" width="20px" color="#219653" />
 	</a>
 </div>
