c11b73bfbb2ede12e33f83494a5005c166107312
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-07-07 17:28:04 +0530

    gap-y-8

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,24 +6,23 @@
 
 <Seo preloads={[{ as: 'image', href: 'https://ik.imagekit.io/vjeqenuhn/static/headshot.jpeg' }]} />
 
-<div class="flex flex-col">
+<div class="flex flex-col gap-y-8">
 	<img
-		width="100"
-		height="100"
-		class="size-[100px] rounded-full"
+		width="75px"
+		height="75px"
+		class="size-[75px] rounded-full"
 		alt="Rishi Raj Jain in his favourite attire"
 		src="https://ik.imagekit.io/vjeqenuhn/static/headshot.jpeg?tr=w-100"
 	/>
-	<h1 class="mt-3 text-2xl font-bold sm:text-5xl">Rishi Raj Jain</h1>
-	<p class="mt-6 text-lg font-light text-slate-600">
+	<h1 class="text-2xl font-bold sm:text-5xl">Rishi Raj Jain</h1>
+	<p class="text-lg font-light text-slate-600">
 		Hey 👋🏻 <br />I am a software engineer and a technical writer. On the side, I'm developing and marketing
 		<a class="text-[#8714bd]" href="https://www.launchfa.st" target="_blank"><u>LaunchFast</u></a>. Feel free to go through my
 		<a class="text-[#ff6719]" href="https://rishirajjain.substack.com" target="_blank"><u>newsletter</u></a>
 		or read my <a class="text-black" href="/blogs"><u>blogs</u></a>.
 	</p>
-	<div class="mt-8 h-[1px] w-full" />
 	<Socials />
-	<ul class="mt-4 flex flex-row flex-wrap gap-4 text-slate-600">
+	<ul class="flex flex-row flex-wrap gap-4 text-slate-600">
 		<li>
 			<a
 				target="_blank"
