c38cba616645fef25eacb08d1f427c6a068bbe05
Rishi <jain71000@gmail.com>
2026-02-16 00:49:14 +0530

    add tripinfox website

---
+++ src/routes/websites/tripinfox/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import DisplayWebsite from '@/src/components/DisplayWebsite.svelte'
+</script>
+
+<DisplayWebsite urls={[{ slug: '/home', url: 'https://drive.google.com/file/d/1nm6M8E5RU8ep8B-SI-aQzND2tDau8P1y' }]} />
